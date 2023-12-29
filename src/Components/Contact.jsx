a26578a71ecde2c0f395b8f77eadbe453e3f1cc3
frs11 <fayezurrahman017@gmail.cpm>
2023-12-30 02:37:35 +0600

    contract form section added with social links

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,8 +1,10 @@
 import { useRef } from "react";
 import emailjs from "@emailjs/browser";
-import facebook from "../assets/social icon/icons8-facebook.svg";
-import github from "../assets/social icon/icons8-github.svg";
-import linkedin from "../assets/social icon/icons8-linkedin.svg";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import { IoMailSharp } from "react-icons/io5";
+import { FaHandHoldingHeart } from "react-icons/fa6";
 
 const Contact = () => {
   const form = useRef();
@@ -29,55 +31,87 @@ const Contact = () => {
   return (
     <div
       id="contact"
-      className="w-11/12 md:w-10/12 lg:w-8/12 mx-auto bg-slate-800 bg-opacity-50 rounded-lg text-white px-5 py-4  border border-red-700"
+      className="w-11/12 lg:w-8/12 mx-auto mb-5 border border-cyan-900 bg-slate-900 bg-opacity-30 rounded-lg text-white px-5 py-4"
     >
       <p className="text-2xl md:text-4xl text-center my-2 md:my-10 font-Orbitron font-medium text-cyan-300">
         <span className=" border-b-2 border-cyan-300 px-2 py-1 rounded-lg">
           Contact
         </span>
       </p>
-      <div className="flex justify-around items-center mt-10 border border-red-700">
-        <div>
-          <p>Lets stay Connected </p>
-          <div className="flex justify-center my-5 space-x-2 text-center">
+      <div className="flex flex-col md:flex-row mx-auto py-2 px-3 justify-around items-center md:mt-12">
+        <div className="w-full py-5 px-3 md:w-1/2">
+          <p className="text-2xl md:text-3xl text-slate-200 font-thin mb-10 flex items-center justify-center md:justify-start">
+            Lets stay Connected{" "}
+            <FaHandHoldingHeart className="ml-2 text-cyan-400"></FaHandHoldingHeart>{" "}
+          </p>
+          <div className="flex justify-center md:justify-start px-5 my-5 space-x-3 text-center">
             <a
               href="https://github.com/frs11"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <img src={github} className="w-[50px]" alt="" />
+              <FaGithub className="text-4xl text-slate-400 hover:text-white"></FaGithub>
+              {/* <img src={github} alt="" /> */}
             </a>
             <a
               href="https://www.linkedin.com/in/fayezur-rahman/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <img src={linkedin} className="w-[50px]" alt="" />
+              <FaLinkedin className="text-4xl text-slate-400 hover:text-white"></FaLinkedin>
+              {/* <img src={linkedin} className="w-[50px]" alt="" /> */}
             </a>
             <a
               href="https://www.facebook.com/fayezurrahman017"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <img src={facebook} className="w-[50px]" alt="" />
+              <FaFacebookSquare className="text-4xl text-slate-400 hover:text-white"></FaFacebookSquare>
+              {/* <img src={facebook} className="w-[50px]" alt="" /> */}
+            </a>
+            <a
+              href="mailto:fayezurrahman017@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {/* <IoIosMail className="text-4xl"></IoIosMail> */}
+              <IoMailSharp className="text-4xl text-slate-400 hover:text-white"></IoMailSharp>
+              {/* <img src={facebook} className="w-[50px]" alt="" /> */}
             </a>
           </div>
         </div>
         <form
           ref={form}
           onSubmit={sendEmail}
-          className="px-5 py-3 bg-slate-950 rounded-lg opacity-70"
+          className="px-5 py-5 my-5 bg-slate-950 w-full md:w-1/2 rounded-lg opacity-70"
         >
-          <label>Name</label>
-          <input type="text" name="user_name" /> <br />
-          <label>Email</label>
-          <input type="email" name="user_email" /> <br />
-          <label>Message</label>
-          <textarea name="message" /> <br />
+          <label className="block text-slate-300">Name</label>
           <input
-            className="px-3 py-1 my-5 border border-cyan-500  rounded-md"
+            type="text"
+            placeholder="Your Name"
+            className="w-full mb-3 px-2 py-1 placeholder:text-sm rounded bg-slate-700 bg-opacity-70"
+            name="user_name"
+          />{" "}
+          <br />
+          <label className="block text-slate-300">Email</label>
+          <input
+            type="email"
+            placeholder="example@email.com"
+            className="w-full mb-3 px-2 py-1 placeholder:text-sm rounded bg-slate-700 bg-opacity-70"
+            name="user_email"
+          />{" "}
+          <br />
+          <label className="block text-slate-300">Message</label>
+          <textarea
+            name="message"
+            placeholder="Type your message"
+            className="w-full mb-3 px-2 py-1 placeholder:text-sm rounded bg-slate-700 bg-opacity-70"
+          />{" "}
+          <br />
+          <input
+            className="px-3 py-2 my-5 border border-cyan-500 rounded-md"
             type="submit"
-            value="Send"
+            value="Send Message"
           />
         </form>
       </div>
