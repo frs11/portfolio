8a2e0041328c9f4378e90a825f96626c37efb306
frs11 <fayezurrahman017@gmail.cpm>
2023-12-31 21:07:41 +0600

    test

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -82,10 +82,10 @@ const Skills = () => {
           <div
             data-aos="flip-right"
             key={idx}
-            className="w-20 h-20 hover:w-24 hover:h-24 px-2 py-2 m-4 hover:my-2 hover:mx-2 duration-150 ease-in-out border bg-cyan-900 bg-opacity-20 text-cyan-500 hover:text-cyan-300 border-cyan-600 hover:border-cyan-400 rounded-md"
+            className="w-20 h-20 hover:w-24 hover:h-24 px-2 py-2 m-4 hover:my-2 hover:mx-2 border bg-cyan-900 bg-opacity-20 text-cyan-500 hover:text-cyan-300 border-cyan-600 hover:border-cyan-400 rounded-md duration-1000"
           >
             <img className="w-full h-full" src={skill.logo} alt="" />
-            <p className="mt-2 text-sm lg:text-base text-center">
+            <p className="mt-3 text-sm lg:text-base text-center">
               {skill.title}
             </p>
           </div>
